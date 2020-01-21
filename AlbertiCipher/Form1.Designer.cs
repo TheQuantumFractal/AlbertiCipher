6c1e5a99abfb72e17937f68cc589320f7bf52ef7
Kamyar Salahi <kamyarusa@yahoo.com>
2020-01-20 20:31:59 -0800

    Initial commit

---
+++ AlbertiCipher/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace AlbertiCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.para = new System.Windows.Forms.TextBox();
+            this.en = new System.Windows.Forms.Button();
+            this.de = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.seed = new System.Windows.Forms.TextBox();
+            this.rot = new System.Windows.Forms.ComboBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.test = new System.Windows.Forms.Button();
+            this.tbox = new System.Windows.Forms.TextBox();
+            this.test2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // para
+            // 
+            this.para.Location = new System.Drawing.Point(12, 44);
+            this.para.Multiline = true;
+            this.para.Name = "para";
+            this.para.Size = new System.Drawing.Size(441, 179);
+            this.para.TabIndex = 0;
+            this.para.TabStop = false;
+            this.para.TextChanged += new System.EventHandler(this.para_TextChanged);
+            this.para.KeyDown += new System.Windows.Forms.KeyEventHandler(this.para_KeyDown);
+            // 
+            // en
+            // 
+            this.en.Location = new System.Drawing.Point(471, 22);
+            this.en.Name = "en";
+            this.en.Size = new System.Drawing.Size(75, 23);
+            this.en.TabIndex = 1;
+            this.en.TabStop = false;
+            this.en.Text = "Encrypt";
+            this.en.UseVisualStyleBackColor = true;
+            this.en.Click += new System.EventHandler(this.en_Click);
+            // 
+            // de
+            // 
+            this.de.Location = new System.Drawing.Point(571, 22);
+            this.de.Name = "de";
+            this.de.Size = new System.Drawing.Size(75, 23);
+            this.de.TabIndex = 1;
+            this.de.TabStop = false;
+            this.de.Text = "Decrypt";
+            this.de.UseVisualStyleBackColor = true;
+            this.de.Click += new System.EventHandler(this.de_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ROT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Seed";
+            // 
+            // seed
+            // 
+            this.seed.Location = new System.Drawing.Point(478, 148);
+            this.seed.Name = "seed";
+            this.seed.Size = new System.Drawing.Size(121, 22);
+            this.seed.TabIndex = 2;
+            // 
+            // rot
+            // 
+            this.rot.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rot.FormattingEnabled = true;
+            this.rot.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25"});
+            this.rot.Location = new System.Drawing.Point(478, 83);
+            this.rot.Name = "rot";
+            this.rot.Size = new System.Drawing.Size(121, 24);
+            this.rot.TabIndex = 1;
+            this.rot.KeyDown += new System.Windows.Forms.KeyEventHandler(this.rot_KeyDown);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(12, 235);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(441, 179);
+            this.result.TabIndex = 0;
+            this.result.TabStop = false;
+            this.result.KeyDown += new System.Windows.Forms.KeyEventHandler(this.result_KeyDown);
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(462, 189);
+            this.test.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(59, 34);
+            this.test.TabIndex = 7;
+            this.test.TabStop = false;
+            this.test.Text = "Test";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            this.test.KeyDown += new System.Windows.Forms.KeyEventHandler(this.test_KeyDown);
+            // 
+            // tbox
+            // 
+            this.tbox.Location = new System.Drawing.Point(459, 227);
+            this.tbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbox.Multiline = true;
+            this.tbox.Name = "tbox";
+            this.tbox.ReadOnly = true;
+            this.tbox.Size = new System.Drawing.Size(200, 194);
+            this.tbox.TabIndex = 0;
+            this.tbox.TabStop = false;
+            this.tbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbox_KeyDown);
+            // 
+            // test2
+            // 
+            this.test2.Location = new System.Drawing.Point(526, 189);
+            this.test2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.test2.Name = "test2";
+            this.test2.Size = new System.Drawing.Size(132, 34);
+            this.test2.TabIndex = 7;
+            this.test2.TabStop = false;
+            this.test2.Text = "Test with Seed";
+            this.test2.UseVisualStyleBackColor = true;
+            this.test2.Click += new System.EventHandler(this.test2_Click);
+            this.test2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.test_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 423);
+            this.Controls.Add(this.tbox);
+            this.Controls.Add(this.test2);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.rot);
+            this.Controls.Add(this.seed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.de);
+            this.Controls.Add(this.en);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.para);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Alberti Cipher Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox para;
+        private System.Windows.Forms.Button en;
+        private System.Windows.Forms.Button de;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox seed;
+        private System.Windows.Forms.ComboBox rot;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.TextBox tbox;
+        private System.Windows.Forms.Button test2;
+    }
+}
+
---
+++ AlbertiCipher/Form1.cs
@@ -0,0 +1,577 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlbertiCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rand;
+        string parag;
+        string[,] value = { { }, { } };
+        string hi;
+        string[,] tevalue;
+
+        DiskGenerator disk = new DiskGenerator();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rand = new Random();
+            //string[,] disc = disk.NoGen(rand);
+            //string var = "";
+            //for(int i = 0; i < 26; i++)
+            //{
+            //    var += String.Format("{0} {1} ", disc[0, i], disc[1, i]);
+            //}
+            //MessageBox.Show(var);
+            rot.Text = "0";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void en_Click(object sender, EventArgs e)
+        {
+            parag = string.Empty;
+            fix();
+            if(seed.Text == string.Empty)
+            {
+                if (rot.Text == "0" || rot.Text == "")
+                {
+                    value = disk.NoGen(rand, 0);
+                }
+                else
+                {
+                    value = disk.NoGen(rand, int.Parse(rot.Text));
+                }
+                seed.Text = disk.seedO.ToString();
+            }
+            else
+            {
+                if (rot.Text == "0" || rot.Text == "")
+                {
+                    value = disk.Gen(rand, int.Parse(seed.Text), 0);
+                }
+                else
+                {
+                    value = disk.Gen(rand, int.Parse(seed.Text), int.Parse(rot.Text));
+                }
+            }
+            disk.reset();
+            for (int i = 0; i < para.Text.Count(); i++)
+            {
+                if (!Char.IsLetter(para.Text[i]))
+                {
+                    parag += para.Text[i];
+                    continue;
+                }
+                else
+                {
+                    for (int x = 0; x < 26; x++)
+                    {
+                        if (Char.ToUpper(para.Text[i]) == Convert.ToChar(value[0, x]))
+                        {
+                            parag += Char.ToLower(Convert.ToChar(value[0, int.Parse(value[1, x])]));
+                            break;
+                        }
+                    }
+                }
+            }
+            result.Text = parag;
+        }
+        public void fix()
+        {
+            //seeder = seed.Text;
+            //if (seeder.Contains('A'))
+            //{
+            //    seeder.Replace('A', '3');
+            //}
+            //if (seeder.Contains('B'))
+            //{
+            //    seeder.Replace('B', '2');
+            //}
+            //if (seeder.Contains('C'))
+            //{
+            //    seeder.Replace('C', '4');
+            //}
+            //if (seeder.Contains('D'))
+            //{
+            //    seeder.Replace('D', '0');
+            //}
+            //if (seeder.Contains('E'))
+            //{
+            //    seeder.Replace('E', '5');
+            //}
+            //if (seeder.Contains('F'))
+            //{
+            //    seeder.Replace('F', '8');
+            //}
+            //if (seeder.Contains('G'))
+            //{
+            //    seeder.Replace('G', '6');
+            //}
+            //if (seeder.Contains('H'))
+            //{
+            //    seeder.Replace('H', '7');
+            //}
+            //if (seeder.Contains('I'))
+            //{
+            //    seeder.Replace('I', '9');
+            //}
+            //if (seeder.Contains('J'))
+            //{
+            //    seeder.Replace('J', '1');
+            //}
+            //if (seeder.Contains('K'))
+            //{
+            //    seeder.Replace('K', '5');
+            //}
+            //if (seeder.Contains('L'))
+            //{
+            //    seeder.Replace('L', '4');
+            //}
+            //if (seeder.Contains('M'))
+            //{
+            //    seeder.Replace('M', '8');
+            //}
+            //if (seeder.Contains('N'))
+            //{
+            //    seeder.Replace('N', '9');
+            //}
+            //if (seeder.Contains('O'))
+            //{
+            //    seeder.Replace('O', '0');
+            //}
+            //if (seeder.Contains('P'))
+            //{
+            //    seeder.Replace('P', '2');
+            //}
+            //if (seeder.Contains('Q'))
+            //{
+            //    seeder.Replace('Q', '7');
+            //}
+            //if (seeder.Contains('R'))
+            //{
+            //    seeder.Replace('R', '4');
+            //}
+            //if (seeder.Contains('S'))
+            //{
+            //    seeder.Replace('S', '6');
+            //}
+            //if (seeder.Contains('T'))
+            //{
+            //    seeder.Replace('T', '1');
+            //}
+            //if (seeder.Contains('U'))
+            //{
+            //    seeder.Replace('U', '2');
+            //}
+            //if (seeder.Contains('V'))
+            //{
+            //    seeder.Replace('V', '0');
+            //}
+            //if (seeder.Contains('W'))
+            //{
+            //    seeder.Replace('W', '8');
+            //}
+            //if (seeder.Contains('X'))
+            //{
+            //    seeder.Replace('X', '5');
+            //}
+            //if (seeder.Contains('Y'))
+            //{
+            //    seeder.Replace('Y', '9');
+            //}
+            //if (seeder.Contains('Z'))
+            //{
+            //    seeder.Replace('Z', '3');
+            //}
+            //if (seeder.Contains('e'))
+            //{
+            //    seeder.Replace('e', '2');
+            //}
+            //if (seeder.Contains('z'))
+            //{
+            //    seeder.Replace('z', '2');
+            //}
+            //if (seeder.Contains('y'))
+            //{
+            //    seeder.Replace('t', '4');
+            //}
+            //if (seeder.Contains('q'))
+            //{
+            //    seeder.Replace('q', '4');
+            //}
+            //if (seeder.Contains('a'))
+            //{
+            //    seeder.Replace('a', '3');
+            //}
+            //if (seeder.Contains('x'))
+            //{
+            //    seeder.Replace('x', '3');
+            //}
+            //if (seeder.Contains('o'))
+            //{
+            //    seeder.Replace('o', '1');
+            //}
+            //if (seeder.Contains('j'))
+            //{
+            //    seeder.Replace('j', '1');
+            //}
+            //if (seeder.Contains('i'))
+            //{
+            //    seeder.Replace('i', '8');
+            //}
+            //if (seeder.Contains('k'))
+            //{
+            //    seeder.Replace('k', '8');
+            //}
+            //if (seeder.Contains('n'))
+            //{
+            //    seeder.Replace('n', '7');
+            //}
+            //if (seeder.Contains('v'))
+            //{
+            //    seeder.Replace('v', '7');
+            //}
+            //if (seeder.Contains('s'))
+            //{
+            //    seeder.Replace('s', '5');
+            //}
+            //if (seeder.Contains('b'))
+            //{
+            //    seeder.Replace('b', '5');
+            //}
+            //if (seeder.Contains('h'))
+            //{
+            //    seeder.Replace('h', '6');
+            //}
+            //if (seeder.Contains('p'))
+            //{
+            //    seeder.Replace('p', '6');
+            //}
+            //if (seeder.Contains('r'))
+            //{
+            //    seeder.Replace('r', '9');
+            //}
+            //if (seeder.Contains('y'))
+            //{
+            //    seeder.Replace('y', '9');
+            //}
+            //if (seeder.Contains('d'))
+            //{
+            //    seeder.Replace('d', '0');
+            //}
+            //if (seeder.Contains('g'))
+            //{
+            //    seeder.Replace('g', '0');
+            //}
+            //if (seeder.Contains('l'))
+            //{
+            //    seeder.Replace('l', '8');
+            //}
+            //if (seeder.Contains('f'))
+            //{
+            //    seeder.Replace('f', '8');
+            //}
+            //if (seeder.Contains('c'))
+            //{
+            //    seeder.Replace('c', '6');
+            //}
+            //if (seeder.Contains('w'))
+            //{
+            //    seeder.Replace('w', '6');
+            //}
+            //if (seeder.Contains('u'))
+            //{
+            //    seeder.Replace('u', '7');
+            //}
+            //if (seeder.Contains('m'))
+            //{
+            //    seeder.Replace('m', '7');
+            //}
+        }
+
+        private void de_Click(object sender, EventArgs e)
+        {
+            string parag2 = "";
+            fix();
+            string[,] devalue;
+            if (seed.Text == string.Empty)
+            {
+                MessageBox.Show("There is no seed!", "NO SEED!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else {
+                int tempseed;
+                if(int.TryParse(seed.Text, out tempseed))
+                {
+                    if (rot.Text == string.Empty || rot.Text == "0")
+                    {
+                        devalue = disk.Degen(rand, tempseed, 0);
+                    }
+                    else
+                    {
+                        devalue = disk.Degen(rand, tempseed, int.Parse(rot.Text));
+                    }
+                    disk.reset();
+                    for (int i = 0; i < para.Text.Count(); i++)
+                    {
+                        if (!Char.IsLetter(para.Text[i]))
+                        {
+                            parag2 += para.Text[i];
+                            continue;
+                        }
+                        else
+                        {
+                            for (int x = 0; x < 26; x++)
+                            {
+                                if (int.Parse(devalue[1, x]) < int.Parse(rot.Text))
+                                {
+                                    if (Char.ToUpper(para.Text[i]) == Convert.ToChar(devalue[0, int.Parse(devalue[1, x])]))
+                                    {
+                                        parag2 += Char.ToLower(Convert.ToChar(devalue[0, x]));
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    if (Char.ToUpper(para.Text[i]) == Convert.ToChar(devalue[0, int.Parse(devalue[1, x])]))
+                                    {
+                                        parag2 += Char.ToLower(Convert.ToChar(devalue[0, x]));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    result.Text = parag2;
+                }
+                else
+                {
+                    MessageBox.Show("You may not use letters in your seed");
+                }
+            }
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            seed.Text = string.Empty;
+            tbox.Text = string.Empty;
+            tevalue = disk.NoGen(rand, int.Parse(rot.Text));
+            seed.Text = disk.seedO.ToString();
+            hi = "";
+            disk.reset();
+            for (int f = 0; f < 26; f++)
+            {
+                hi += tevalue[0, f];
+                hi += "=";
+                hi += tevalue[0, int.Parse(tevalue[1, f])];
+                if (f % 4 == 0 && f > 0)
+                {
+                    hi += "\r\n";
+                }
+                else
+                {
+                    hi += "   ";
+                }
+            }
+            tbox.Text = hi;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                if(int.Parse(rot.Text) < 25)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) + 1);
+                }
+                else
+                {
+                    rot.Text = "1";
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (int.Parse(rot.Text) > 1)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) - 1);
+                }
+                else
+                {
+                    rot.Text = "25";
+                }
+            }
+        }
+
+        private void rot_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (int.Parse(rot.Text) < 25)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) + 1);
+                }
+                else
+                {
+                    rot.Text = "1";
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (int.Parse(rot.Text) > 1)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) - 1);
+                }
+                else
+                {
+                    rot.Text = "25";
+                }
+            }
+        }
+
+        private void para_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (int.Parse(rot.Text) < 25)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) + 1);
+                }
+                else
+                {
+                    rot.Text = "1";
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (int.Parse(rot.Text) > 1)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) - 1);
+                }
+                else
+                {
+                    rot.Text = "25";
+                }
+            }
+        }
+
+        private void test_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (int.Parse(rot.Text) < 25)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) + 1);
+                }
+                else
+                {
+                    rot.Text = "1";
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (int.Parse(rot.Text) > 1)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) - 1);
+                }
+                else
+                {
+                    rot.Text = "25";
+                }
+            }
+        }
+
+        private void tbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (int.Parse(rot.Text) < 25)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) + 1);
+                }
+                else
+                {
+                    rot.Text = "1";
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (int.Parse(rot.Text) > 1)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) - 1);
+                }
+                else
+                {
+                    rot.Text = "25";
+                }
+            }
+        }
+
+        private void result_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (int.Parse(rot.Text) < 25)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) + 1);
+                }
+                else
+                {
+                    rot.Text = "1";
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (int.Parse(rot.Text) > 1)
+                {
+                    rot.Text = Convert.ToString(int.Parse(rot.Text) - 1);
+                }
+                else
+                {
+                    rot.Text = "25";
+                }
+            }
+        }
+
+        private void test2_Click(object sender, EventArgs e)
+        {
+            tbox.Text = string.Empty;
+            tevalue = disk.Gen(rand, int.Parse(seed.Text), int.Parse(rot.Text));
+            hi = "";
+            disk.reset();
+            for (int f = 0; f < 26; f++)
+            {
+                hi += tevalue[0, f];
+                hi += "=";
+                hi += tevalue[0, int.Parse(tevalue[1, f])];
+                if (f % 4 == 0 && f > 0)
+                {
+                    hi += "\r\n";
+                }
+                else
+                {
+                    hi += "   ";
+                }
+            }
+            tbox.Text = hi;
+        }
+
+        private void para_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
